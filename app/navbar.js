c82a703b5df80e9d108386a337f0cf947721fd39
ulapkus <ulapkus@gmail.com>
2024-02-17 00:36:36 +0700

    experimented with parallax scrolling

---
+++ app/navbar.js
@@ -0,0 +1,30 @@
+// "use client";
+
+// import React from "react";
+// import Link from "next/link";
+
+// const Navbar = () => {
+//   return (
+//     <div className="nav-background">
+//       <Link href="/about" className="nav-kriste">
+//         KRISTE AUKŠTUOLIS
+//       </Link>
+//       <div className="main-links">
+//         <Link href="/about" className="nav-link">
+//           WORKS
+//         </Link>{" "}
+//         <Link href="/about" className="nav-link">
+//           ABOUT
+//         </Link>{" "}
+//         <Link href="/about" className="nav-link">
+//           CONTACT
+//         </Link>
+//         <Link href="/about" className="nav-link">
+//           INSTAGRAM
+//         </Link>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Navbar;
